278b77bd75cf481cc155914a68c9d747de401a81
Pavel Averin <pasha2281337alfa@gmail.com>
2024-04-28 13:34:15 +0300

    feat: add metric and stats gathering for individual ticks

--- pfuzz/gem5_statistics/tick_statistics.py
+++ pfuzz/gem5_statistics/tick_statistics.py
@@ -1,5 +1,6 @@
+import m5
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List
 
 
 class Tick_statistics:
@@ -111,7 +112,7 @@ class Tick_statistics:
         self.update_so_far_stages_numbers(ticks, tick_number)
         return tick_number
 
-    def get_tick_statistics_list(self, path_to_trace: str) -> Dict[str, _Tick]:
+    def get_tick_statistics_list(self, path_to_trace: str) -> List[_Tick]:
         """
         Function for aquiring the information for each tick from a given trace
         in a form of a dictionary of instances of Tick dataclass
@@ -131,4 +132,38 @@ class Tick_statistics:
                 old_tick = current_tick
                 current_tick = self.proccess_stage(line, ticks, old_tick)
 
-        return ticks
+        tick_list = list(ticks.values())
+        tick_list.sort(key=lambda x: x.tick_number)
+        return tick_list
+
+    def get_longest_tick_sequence_without_retired(
+        self,
+        ticks_list: List[_Tick],
+    ) -> List[int]:
+        count, count_max = 0, 0
+        tick_sequence, longest_tick_sequence = [], []
+
+        for tick in ticks_list:
+            if tick.retired_during_this_tick == 0:
+                count += 1
+                tick_sequence.append(tick.tick_number)
+            else:
+                if count >= count_max:
+                    count = count_max
+                    longest_tick_sequence = tick_sequence
+                    tick_sequence = []
+                    count = 0
+        longest_tick_sequence.reverse()
+        return longest_tick_sequence
+
+    def get_stats_for_certain_ticks_exclusively(self, ticks: List[int]) -> None:
+        """Function to be put into simulation to generate stats for each
+        tick exclusively in stats.txt (not commulative stats)"""
+        for tick in ticks:
+            while True:
+                if m5.curTick() == tick - 1000:
+                    m5.stats.reset()
+                if m5.curTick() == tick:
+                    m5.stats.dump()
+                    break
+                m5.simulate(1000)
